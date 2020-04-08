bfad0bf3e19f8706b80486eafe3f34ef44638a1e
Kishore Karanam <kishorekaranam99@gmail.com>
2020-04-08 09:58:34 +0100

    Day 8

---
+++ Leetcode/30 DAY Challenge/midOfLL.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class midOfLL {
+    public ListNode middleNode(ListNode head) {
+        ListNode a = head;
+        ListNode b = head;
+        while(b!=null && b.next!=null){
+            a = a.next;
+            b = b.next.next;
+        }
+        return a;
+    }
+}
