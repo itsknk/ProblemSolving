53d63d4e6b9f8969e0f66febceaaf37bc1d02626
Kishore Karanam <kishorekaranam99@gmail.com>
2020-04-20 15:27:09 +0100

    No:997 Diff:easy

---
+++ Leetcode/findTownJudge.java
@@ -0,0 +1,15 @@
+class findTownJudge {
+    public int findJudge(int N, int[][] trust) {
+        int[] count = new int[N+1];
+        for(int[] i : trust){
+            count[i[0]]--;
+            count[i[1]]++;
+        }
+        for(int i =1; i<=N; i++){
+            if(count[i] == N-1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
