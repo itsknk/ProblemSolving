cc6230726bece2e83dd76b9b8f29af823b11f0f9
Kishore Karanam <kishorekaranam99@gmail.com>
2020-05-04 07:43:32 +0000

    No:476 Diff:easy

---
+++ Leetcode/numberComplement.java
@@ -0,0 +1,12 @@
+class numberComplement {
+    public int findComplement(int num) {
+        int ans =0;
+        int power = 1;
+        while(num > 0){
+            ans+=(num%2 ^ 1) * power;
+            power <<=1;
+            num>>=1;
+        }
+        return ans;
+    }
+}
