1259c40f22460164f758b91427a9959d02aaa831
Kishore Karanam <kishorekaranam99@gmail.com>
2020-05-05 11:38:32 +0000

    No:455 Diff:easy

---
+++ Leetcode/assignCookies.java
@@ -0,0 +1,20 @@
+class assignCookies {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count =0;
+        int i = g.length-1;
+        int j = s.length-1;
+        while(i>=0 && j>=0){
+            if(s[j] >= g[i]){
+                count++;
+                i--;
+                j--;
+            }
+            else{
+                i--;
+            }
+        }
+        return count;
+    }
+}
