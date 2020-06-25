791f4fde5236b63e16e74c8ba3588c0c197f8266
Kishore Karanam <kishorekaranam99@gmail.com>
2020-06-25 22:01:47 +0530

    No:287 Diff:medium

---
+++ Leetcode/findTheDuplicate.java
@@ -0,0 +1,10 @@
+class findTheDuplicate {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==nums[i+1])
+              return nums[i];
+        }
+        return 0;
+    }
+}
