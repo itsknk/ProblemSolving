2e5fae5ca5ea233ce4056ebde021a052cb7e6f7c
Kishore Karanam <kishorekaranam99@gmail.com>
2020-07-03 19:36:45 +0530

    No:206 Diff:easy

--- Leetcode/reverseLinkedList.java
+++ Leetcode/reverseLinkedList.java
@@ -1,5 +1,6 @@
 class reverseLinkedList {
     public ListNode reverseList(ListNode head) {
+        //direct iteration
        ListNode current = head;
        ListNode next = null;
        ListNode prev = null;
@@ -10,5 +11,19 @@ class reverseLinkedList {
             current = next;
         }
         return prev;
+        //Stack approach
+        Stack<ListNode> stack = new Stack<ListNode>();
+      while(head!=null){
+          stack.push(head);
+          head = head.next;
+      }
+        ListNode temp = new ListNode(-1);
+        head = temp;
+        while(!stack.isEmpty()){
+            ListNode current = stack.pop();
+            head.next = new ListNode(current.val);
+            head = head.next;
+        }
+        return temp.next;
     }
 }
