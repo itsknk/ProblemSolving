5cec92a79ab3efad4e6a1b168d12351b08223c74
Kishore Karanam <kishorekaranam99@gmail.com>
2020-08-14 20:48:02 +0530

    No:409 Diff:easy

---
+++ Leetcode/longestPalindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int [] counts = new int[128];
+        for(char c: s.toCharArray()){
+            counts[c]++;
+        }
+        int result = 0;
+        for(Integer count :  counts){
+            result += count/2*2;
+            if(result % 2 == 0 && count %2 == 1){
+                result += 1;
+            }
+        }
+        return result;
+    }
+}
