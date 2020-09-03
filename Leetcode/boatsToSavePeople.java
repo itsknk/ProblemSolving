1ac65149d8ebfdd69407d43e450e84a274e1c409
Kishore Karanam <kishorekaranam99@gmail.com>
2020-09-03 23:14:39 +0530

    No:881 Diff:medium

---
+++ Leetcode/boatsToSavePeople.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int light=0;
+        int heavy=people.length-1;
+        int boats=0;
+        while(light<=heavy){
+            if(people[light]+people[heavy] <= limit){
+                light++;
+                heavy--;
+            }else{
+                heavy--;
+            }
+            boats++;
+        }
+        return boats;
+    }
+}
