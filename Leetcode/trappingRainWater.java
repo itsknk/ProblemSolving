7c66691e337757e91c1250601700890f20764f6b
Kishore Karanam <kishorekaranam99@gmail.com>
2020-12-09 19:12:23 +0530

    No:42 Diff:hard

--- Leetcode/trappingRainWater.java
+++ Leetcode/trappingRainWater.java
@@ -12,3 +12,30 @@ class Solution {
         return result;
     }
 }
+
+//0ms
+class Solution {
+    public int trap(int[] height) {
+        if(height.length == 0)
+            return 0;
+        int low =0, high = height.length-1, ans=0;
+        int left = height[low];
+        int right = height[high];
+        while(low <= high)
+        {
+            left = Math.max(left, height[low]);
+            right = Math.max(right, height[high]);
+            if(left<right)
+            {
+                ans += left - height[low];
+                low++;
+            }
+            else
+            {
+                ans += right - height[high];
+                high--;
+            }
+        }
+        return ans;
+    }
+}
