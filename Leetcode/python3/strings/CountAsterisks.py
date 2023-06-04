6980c5ca9e4445a9c413749a38f2bcf7f2c21923
itsknk <kishorekaranam99@gmail.com>
2023-06-04 14:04:44 -0700

    Better Organised

--- Leetcode/python3/strings/CountAsterisks.py
+++
@@ -1,9 +0,0 @@
-# Split by bar then consider only even location data and count * there.
-class Solution:
-    def countAsterisks(self, s: str) -> int:
-        a = s.split("|")
-        count = 0
-        for i,j in enumerate(a):
-            if i%2 == 0:
-                count = count + j.count("*")
-        return count
